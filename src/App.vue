355f0f64313df9b5d477f519092fe1cba28f6ef6
Josias Ruegg <josiasruegg@gmail.com>
2020-08-14 00:10:40 -0500

    home implemented #1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
 <template>
   <v-app>
-    RANK-UNI app here
+    <v-app-bar app color="teal" dense>
+      <v-btn icon to="/home">
+        <v-icon>
+          mdi-home
+        </v-icon>
+      </v-btn>
+
+      <v-toolbar-title>Rank-UNI</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>
+          mdi-account
+        </v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+
   </v-app>
 </template>
 
---
+++ src/components/Cursos.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Cursos',
+
+  components: {
+  },
+
+  data: () => ({
+  }),
+};
+
+</script>
+
+<style>
+</style>
