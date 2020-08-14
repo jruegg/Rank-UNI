e08d70a0fcf471e0107333b54a22b95ace71edbf
Josias Ruegg <josiasruegg@gmail.com>
2020-08-14 08:56:48 -0500

    implemented rank views, see #3

--- src/components/Fc.vue
+++ src/components/Fc.vue
@@ -1,5 +1,31 @@
 <template>
   <div>
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-card raised height="100%">
+            <v-card-title>Distribución FC</v-card-title>
+            <v-sparkline
+              fill
+              height="150"
+              :gradient="['#17d1a3', '#c1e3db']"
+              :smooth="6"
+              :value="value"
+              auto-draw
+            ></v-sparkline>
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col>
+          <v-card raised height="100%">
+            <v-card-title>Top FC</v-card-title>
+            <apexchart type="bar" height="200" :options="chartOptions" :series="series"></apexchart>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
@@ -12,6 +38,73 @@ export default {
   },
 
   data: () => ({
+    value: [1, 1, 3, 4, 5, 4, 9, 4, 2, 1],
+    series: [{
+      data: [17.7, 17.6, 17.6, 17.5, 17.5],
+    }],
+    chartOptions: {
+      chart: {
+        type: 'bar',
+        toolbar: {
+          show: false,
+        },
+      },
+      plotOptions: {
+        bar: {
+          barHeight: '100%',
+          distributed: true,
+          horizontal: true,
+          dataLabels: {
+            position: 'bottom',
+          },
+        },
+      },
+      colors: ['#cc33b8'],
+      dataLabels: {
+        enabled: true,
+        textAnchor: 'start',
+        style: {
+          colors: ['#fff'],
+        },
+        formatter(val, opt) {
+          return `${opt.w.globals.labels[opt.dataPointIndex]}:  ${val}`;
+        },
+        offsetX: 0,
+        dropShadow: {
+          enabled: true,
+        },
+      },
+      stroke: {
+        width: 1,
+        colors: ['#fff'],
+      },
+      xaxis: {
+        categories: ['N6', 'N2', 'N1', 'N3', 'N4'],
+      },
+      yaxis: {
+        labels: {
+          show: false,
+        },
+        min: 0,
+        max: 20,
+      },
+      tooltip: {
+        theme: 'dark',
+        x: {
+          show: false,
+        },
+        y: {
+          title: {
+            formatter() {
+              return '';
+            },
+          },
+        },
+      },
+      legend: {
+        show: false,
+      },
+    },
   }),
 };
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import Home from '../components/Home.vue';
+import Fc from '../components/Fc.vue';
+import Uni from '../components/Uni.vue';
+import Cc from '../components/Cc.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +12,18 @@ const routes = [
   {
     path: '/home',
     component: Home,
+  },
+  {
+    path: '/uni',
+    component: Uni,
+  },
+  {
+    path: '/fc',
+    component: Fc,
+  },
+  {
+    path: '/cc',
+    component: Cc,
   },
 ];
 
